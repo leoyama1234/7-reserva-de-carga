788f831833cd521fa1a83ebbecd0f9fc85f61d7a
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-09 11:51:37 -0300

    exercicio 07

---
+++ src/br/fiap/cliente/Cliente.java
@@ -0,0 +1,21 @@
+package br.fiap.cliente;
+
+public class Cliente {
+    private int cnpj;
+    private String cliente;
+
+    public Cliente(int cnpj, String cliente){
+        this.cnpj = cnpj;
+        this.cliente = cliente;
+    }
+
+    public String getCliente(){
+        return cliente;
+    }
+
+    public void setCliente(String cliente){
+        this.cliente = cliente;
+    }
+
+
+}
