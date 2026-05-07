9c8c779d764cbbd127383f718ea0a203b0a17502
Laboratório FIAP <labsfiap@fiap.com.br>
2026-05-07 11:46:57 -0300

    exercicio finalizado

--- src/br/fiap/util/Util.java
+++ src/br/fiap/util/Util.java
@@ -35,17 +35,26 @@ public class Util {
     }
 
     private void cancelar() {
+        int id = parseInt(showInputDialog("ID para busca"));
+        if (viagem.cancelar(id)){
+            showMessageDialog(null,"Carga cancelada com sucesso");
+        }
+        else {
+            showMessageDialog(null, "Erro ao cancelar a carga");
+        }
     }
 
     private void pesquisar() {
-    }
-
-    private void pesquisarId(){
-        int id = parseInt(showInputDialog("ID para pesquisa"));
-        for (int i = 0; i < viagem.getIndex(); i++) {
-
+        int id = parseInt(showInputDialog("ID para busca"));
+        Carga carga = viagem.pesquisar(id);
+        if (carga == null) {
+            showMessageDialog(null,"Carga não encontrada");
+        }
+        else {
+            showMessageDialog(null,carga.getDados());
         }
     }
+
 
     private void exibir() {
         showMessageDialog(null, viagem.getDados());
--- src/br/fiap/viagem/Viagem.java
+++ src/br/fiap/viagem/Viagem.java
@@ -13,6 +13,33 @@ public class Viagem {
         this.carga = new Carga[20];
     }
 
+    public Carga pesquisar(int id){
+        int i = pesquisarId(id);
+        if (i != -1){
+            return carga[i];
+        }
+        return null;
+    }
+
+    public boolean cancelar(int id){
+        int i = pesquisarId(id);
+        if (i != -1){
+            carga[i] = carga[index - 1];
+            index--;
+            return true;
+        }
+        return false;
+    }
+
+    private int pesquisarId(int id) {
+        for (int i = 0; i < index; i++) {
+            if (carga[i].getId() == id){
+                return i;
+            }
+        }
+        return -1;
+    }
+
     public double capacidadeReservada(){
         double total = 0;
         for (int i = 0; i < index; i++){
